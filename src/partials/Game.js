738d43fe305ff5ed71365cbea3dae8e3b1bb5451
DanielKell <danielkell.ubc@gmail.com>
2017-08-03 15:27:13 -0700

    Setup both players on board
    -Added both players (paddles) to the board. Setup some variables and
    use them to define new Paddle objects. Instantiated both.

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,8 +1,8 @@
 import {
-	SVG_NS
+	SVG_NS, KEYS
 } from '../settings.js';
 import Board from './Board.js';
-import Paddle from './Paddle.js'
+import Paddle from './Paddle.js';
 
 export default class Game {
 
@@ -10,9 +10,14 @@ export default class Game {
 		this.width = width;
 		this.height = height;
 
+		this.paddleWidth = 8;
+		this.paddleHeight = 56;
+		this.boardGap = 10;
+
 		this.gameElement = document.getElementById(element) //Store a reference to the element we are attaching the game to.
 		this.board = new Board(this.width, this.height); //Instantiated the board
-		this.paddle = new Paddle('256', '5', '20', '10', '10');
+		this.player1 = new Paddle(this.height, this.paddleWidth, this.paddleHeight, this.boardGap, (this.height - this.paddleHeight)/2);
+		this.player2 = new Paddle(this.height, this.paddleWidth, this.paddleHeight, (this.width - this.boardGap - this.paddleWidth), (this.height - this.paddleHeight)/2);
 	}
 
 	render() { //Render method is drawing the SVGs. Here we render the outermost SVG element.
@@ -25,6 +30,7 @@ export default class Game {
 		this.gameElement.appendChild(svg); //Appending to div with an id of game.
 		this.board.render(svg); //Connecting board to svg. In render method it's accepting argument of svg, and creating the pieces inside the board.
 		//Passing in the above let svg
-		this.paddle.render(svg); //Rendering the paddle on board
+		this.player1.render(svg); //Rendering the paddles on board
+		this.player2.render(svg);
 	}
 }
