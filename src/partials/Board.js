2f26916ab816068f6d6c1b8e7ff88fea1a8efc8b
DanielKell <danielkell.ubc@gmail.com>
2017-08-03 14:40:41 -0700

    Added Board settings
    -Set up SVG in the board.js based on drafted SVG in the html previously.
    -Set up render method in Game.js

---
+++ src/partials/Board.js
@@ -0,0 +1,40 @@
+//We will be rendering a rectangle and a line
+
+import {SVG_NS} from '../settings.js';
+
+export default class Board {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    render(svg) { //When we call this, we are passing in a reference to the svg. 
+
+        	/*<svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 512 256" width="512" height="256">
+				<rect x="0" y="0" width="512" height="256" fill="purple"/> 
+				<rect x="10" y="90" width="8" height="56" fill="white"/> 
+				<rect x="494" y="90" width="8" height="56" fill="white"/>
+				<circle cx="300" cy="125" r="8" fill="white"/> 
+				<line stroke-dasharray="28.5, 28.5" x1="256" y1="0" x2="256" y2="256" stroke="white" stroke-width="10"/>
+				<text x="210" y="30" text-anchor="middle" fill="white" font-size="26">2</text>
+				<text x="300" y="30" text-anchor="middle" fill="white" font-size="26">3</text>
+			</svg>*/
+            let rect = document.createElementNS(SVG_NS, 'rect');
+            rect.setAttributeNS(null, 'width', this.width);
+            rect.setAttributeNS(null, 'height', this.height);
+            rect.setAttributeNS(null, 'x', '0');
+            rect.setAttributeNS(null, 'y', '0');
+            rect.setAttributeNS(null, 'fill', 'purple');
+            svg.appendChild(rect);
+
+            let line = document.createElementNS(SVG_NS, 'line');
+            line.setAttributeNS(null, 'stroke-dasharray', '28.5, 28.5');
+            line.setAttributeNS(null, 'x1', this.width/2);
+            line.setAttributeNS(null, 'x2', this.width/2);
+            line.setAttributeNS(null, 'y1', '0');
+            line.setAttributeNS(null, 'y2', this.height);
+            line.setAttributeNS(null, 'stroke', 'white');
+            line.setAttributeNS(null, 'stroke-width', '10');
+            svg.appendChild(line);
+    }
+}
--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,15 +1,26 @@
+import { SVG_NS} from '../settings.js';
+import Board from './Board.js';
+
 export default class Game {
 
 	constructor(element, width, height) { //Constructor Class
-		this.element = element;
-		this.width = width;
+	    this.width = width;
 		this.height = height;
-	
-		// Other code goes here...
+
+		this.gameElement = document.getElementById(element) //Store a reference to the element we are attaching the game to.
+		this.board = new Board(this.width, this.height); //Instantiated the board
 	}
 
-	render() { //Render method is drawing the SVGs
-		// More code goes here...
+	render() { //Render method is drawing the SVGs. Here we render the outermost SVG element.
+		this.gameElement.innerHTML = ''; //Create empty string to stop if from reloading everything over and over.
+		let svg = document.createElementNS(SVG_NS, 'svg'); //Store newly created element. Need NS as it's a namespace element. Imported SVG_NS above.
+		svg.setAttributeNS(null, 'version', '1.1');
+		svg.setAttributeNS(null, 'width', this.width); //Defining height and width of game board
+		svg.setAttributeNS(null, 'height', this.height);
+		svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`);
+		this.gameElement.appendChild(svg); //Appending to div with an id of game.
+		this.board.render(svg); //Connecting board to svg. In render method it's accepting argument of svg, and creating the pieces inside the board.
+		//Passing in the above let svg
 	}
 
 }
